8f30cbb23d6929135f8d50fdfafca93e000afe8f
Philip Lewis <philipjonlewis@gmail.com>
2022-06-28 11:36:46 +0800

    trying out different project card styles

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -6,6 +6,7 @@ import { motion } from "framer-motion";
 import { ExternalLinkIcon } from "@heroicons/react/solid";
 import { GitHubIcon } from "../../components/branding";
 import Image from "next/image";
+
 const Projects: NextPage = () => {
   const container = {
     hidden: { opacity: 0 },
@@ -26,6 +27,36 @@ const Projects: NextPage = () => {
     },
   };
 
+  const projectContent = [
+    {
+      projectId: 1,
+      projectName: "taptaptask",
+      projectDescription:
+        "taptaptask is a task management tool for phase-based projects.",
+      techStack: [
+        "React",
+        "Redux Toolkit",
+        "RTK Query",
+        "Node",
+        "Express",
+        "MongoDB",
+        "Mongoose",
+        "Scss",
+        "Vite",
+      ],
+      gitHubLinks: [
+        {
+          name: "Front-End",
+          link: "https://www.github.com/_jonlewis",
+        },
+        {
+          name: "Back-End",
+          link: "https://www.github.com/_jonlewis",
+        },
+      ],
+    },
+  ];
+
   return (
     <motion.div className="mt-10">
       <motion.div
@@ -34,61 +65,60 @@ const Projects: NextPage = () => {
         animate="show"
         className="projects-container"
       >
-        <motion.div
-          variants={item}
-          className=" h-fit bg-slate-100 transition rounded-md p-1 hover:shadow-md flex flex-col justify-between "
-        >
-          <div className="flex flex-row justify-between px-2 py-4 border-b-[1px] border-slate-200">
-            <span className="text-xl font-semibold">taptaptask</span>
-            <a href="https://www.taptaptask.com" className="cursor-pointer">
-              <ExternalLinkIcon height={24} />
-            </a>
-          </div>
-          <div className="h-full py-2 flex flex-col justify-between">
-            <div className="image-container ">
-              {" "}
-              <Image src="/taptaptask.webp" layout="fill" className={"image"} />
-            </div>
-            <div className="mb-4 p-2">
-              <p className="text-sm ">
-                taptaptask is a task management tool for phase-based projects.
-              </p>
-              <p className="text-right text-sm mt-2">Read More...</p>
-            </div>
-            <div className="text-xs font-semibold w-full flex gap-1  ">
-              <p className="bg-slate-200 inline-block px-2 py-1 rounded-2xl">
-                React
-              </p>
-              <p className="bg-slate-200 inline-block px-2 py-1 rounded-2xl">
-                Node
-              </p>
-              <p className="bg-slate-200 inline-block px-2 py-1 rounded-2xl">
-                Express
-              </p>
-              <p className="bg-slate-200 inline-block px-2 py-1 rounded-2xl">
-                Scss
-              </p>
-              <p className="bg-slate-200 inline-block px-2 py-1 rounded-2xl">
-                Vite
-              </p>
-            </div>
-          </div>
-          <div className="px-2 py-1 flex justify-end ">
-            {/* <p className="text-xs mr-1">See The Project on Github</p> */}
-            <a href="http://www.github.com/_jonlewis">
-              <GitHubIcon />
-            </a>
-          </div>
-        </motion.div>
-        {/* <motion.div variants={item} className="project-card">
-          archestimator
-        </motion.div>
-        <motion.div variants={item} className="project-card">
-          quicknotes
-        </motion.div>
-        <motion.div variants={item} className="project-card">
-          easymoney
-        </motion.div> */}
+        {projectContent.map((project) => {
+          const {
+            projectId,
+            projectName,
+            projectDescription,
+            techStack,
+            gitHubLinks,
+          } = project;
+          return (
+            <motion.div
+              variants={item}
+              className="project-card"
+              key={projectId}
+            >
+              <div className="title-header">
+                <span className="title">{projectName}</span>
+                <a href="https://www.taptaptask.com" className="cursor-pointer">
+                  <ExternalLinkIcon height={24} />
+                </a>
+              </div>
+              <div className="content-container">
+                {/* <div className="image-container ">
+                  {" "}
+                  <Image
+                    src="/taptaptask.webp"
+                    layout="fill"
+                    className={"image"}
+                  />
+                </div> */}
+                <div className="description-container">
+                  <p>{projectDescription}</p>
+                  <p>Read More...</p>
+                </div>
+                <div className="tech-stack-container">
+                  {techStack.map((stack, index) => {
+                    return <span key={index}>{stack}</span>;
+                  })}
+                </div>
+              </div>
+              <div className="github-link-container">
+                {gitHubLinks.map((link, index) => {
+                  return (
+                    <div key={index}>
+                      <a href={link.link}>
+                        <GitHubIcon />
+                      </a>
+                      <p>{link.name}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </motion.div>
+          );
+        })}
       </motion.div>
     </motion.div>
   );
