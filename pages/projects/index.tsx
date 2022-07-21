6069929b91b9e9018e4535a0e8893750b4ba84e7
Philip Lewis <philipjonlewis@gmail.com>
2022-07-21 13:19:11 +0800

    fixed project details

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -7,7 +7,109 @@ import { ExternalLinkIcon } from "@heroicons/react/solid";
 import { GitHubIcon } from "../../components/branding";
 import Image from "next/image";
 
-import projectContent from "./projectList";
+const projectContent = [
+  {
+    projectId: 1,
+    projectName: "taptaptask",
+    projectDescription:
+      "taptaptask is a task management tool for phase-based projects.",
+    techStack: [
+      "Typescript",
+      "React",
+      "Redux Toolkit",
+      //   "RTK Query",
+      "Node",
+      "Express",
+      "MongoDB",
+      "Mongoose",
+      "Css + Sass",
+      "Vite",
+    ],
+    siteLink: "https://www.taptaptask.com",
+    imageLink: "/taptaptask.webp",
+    gitHubLinks: [
+      {
+        name: "Front-End",
+        link: "https://www.github.com/_jonlewis",
+      },
+      {
+        name: "Back-End",
+        link: "https://www.github.com/_jonlewis",
+      },
+    ],
+  },
+  {
+    projectId: 2,
+    projectName: "archestimator",
+    projectDescription:
+      "archestimator is a construction cost estimator for architects.",
+    techStack: ["Typescript", "Svelte", "Css + Sass", "Rollup"],
+    siteLink: "https://www.archestimator.com",
+    imageLink: "/archestimator.jpeg",
+    gitHubLinks: [
+      {
+        name: "Front-End",
+        link: "https://www.github.com/_jonlewis",
+      },
+    ],
+  },
+  {
+    projectId: 3,
+    projectName: "quicknotes",
+    projectDescription: "quicknotes is a minimalist block style editor",
+    techStack: [
+      "Typescript",
+      "React",
+      "EditorJs",
+      "Css + Sass",
+      "Vite",
+      "Firebase + Firestore",
+    ],
+    siteLink: "https://quicknotesbyphilip.netlify.app",
+    imageLink: "/quicknotes.png",
+    gitHubLinks: [
+      {
+        name: "Front-End",
+        link: "https://www.github.com/_jonlewis",
+      },
+    ],
+  },
+  // {
+  //   projectId: 4,
+  //   projectName: "easymoney",
+  //   projectDescription: "easymoney is a finance management app",
+  //   techStack: [
+  //     "Typescript",
+  //     "React",
+  //     "EditorJs",
+  //     "Css + Sass",
+  //     "Vite",
+  //     "Firebase + Firestore",
+  //   ],
+  //   siteLink: "https://quicknotesbyphilip.netlify.app",
+  //   imageLink: "/easymoney.webp",
+  //   gitHubLinks: [
+  //     {
+  //       name: "Front-End",
+  //       link: "https://www.github.com/_jonlewis",
+  //     },
+  //   ],
+  // },
+  {
+    projectId: 5,
+    projectName: "philipjonlewis",
+    projectDescription: "philipjonlewis portfolio website",
+    techStack: ["Typescript", "React + NextJs", "Tailwind", "Css + Sass"],
+    siteLink: "https://quicknotesbyphilip.netlify.app",
+    imageLink: "/philipjonlewis.png",
+    gitHubLinks: [
+      {
+        name: "Front-End",
+        link: "https://www.github.com/_jonlewis",
+      },
+    ],
+  },
+];
 
 const Projects: NextPage = () => {
   const container = {
