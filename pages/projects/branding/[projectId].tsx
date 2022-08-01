436ac363e6a7d32b176d52acdbc1f38e4c7b5395
Philip Lewis <philipjonlewis@gmail.com>
2022-08-01 14:17:14 +0800

    removed sub pages

--- pages/projects/branding/[projectId].tsx
+++ pages/projects/branding/[projectId].tsx
@@ -7,40 +7,39 @@ import type { ProjectContentFormat } from "../../../db/projects/software/softwar
 import ReactMarkdown from "react-markdown";
 import ProjectsLayout from "../../../components/layouts/ProjectsLayout";
 const Post = ({ projectData }) => {
-  const router = useRouter();
-  const { projectName, projectDescription } = projectData;
-
-  return (
-    <div>
-      <button onClick={() => router.back()}>Go Back</button>
-      <p>Project Name : {projectName}</p>
-      <p>Project Description : {projectDescription}</p>
-      <ReactMarkdown># Hello, *world*!</ReactMarkdown>
-    </div>
-  );
+  // const router = useRouter();
+  // const { projectName, projectDescription } = projectData;
+  // return (
+  //   <div>
+  //     <button onClick={() => router.back()}>Go Back</button>
+  //     <p>Project Name : {projectName}</p>
+  //     <p>Project Description : {projectDescription}</p>
+  //     <ReactMarkdown># Hello, *world*!</ReactMarkdown>
+  //   </div>
+  // );
 };
 
 export default Post;
 
-export async function getStaticPaths() {
-  const paths = projectContent.map((project) => {
-    return { params: { projectId: project.projectId.toString() } };
-  });
+// export async function getStaticPaths() {
+//   const paths = projectContent.map((project) => {
+//     return { params: { projectId: project.projectId.toString() } };
+//   });
 
-  return {
-    paths,
-    fallback: false,
-  };
-}
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
-export async function getStaticProps(context) {
-  const projectId = context.params.projectId;
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/projects/software/${projectId}`
-  );
-  const data = await res.json();
+// export async function getStaticProps(context) {
+//   const projectId = context.params.projectId;
+//   const res = await fetch(
+//     `${process.env.NEXT_PUBLIC_API_URL}/api/projects/software/${projectId}`
+//   );
+//   const data = await res.json();
 
-  return {
-    props: { projectData: data.payload },
-  };
-}
+//   return {
+//     props: { projectData: data.payload },
+//   };
+// }
