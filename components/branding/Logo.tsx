d9a7efd27d72e27781b2efed84700b4493fa11b1
philipjonlewis <philipjonlewis@gmail.com>
2024-01-13 19:58:13 +0800

    Change logo name

--- components/branding/Logo.tsx
+++ components/branding/Logo.tsx
@@ -1,27 +1,27 @@
-import { LightningBoltIcon } from "@heroicons/react/solid";
-import Link from "next/link";
-import { annotate, annotationGroup } from "rough-notation";
-import { useRef, useEffect } from "react";
-import { motion } from "framer-motion";
+import { LightningBoltIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+import { annotate, annotationGroup } from 'rough-notation'
+import { useRef, useEffect } from 'react'
+import { motion } from 'framer-motion'
 
 const Logo = () => {
   const logoVariants = {
     hidden: { opacity: 0 },
     visible: { opacity: 1, transition: { delay: 0.3 } },
-  };
+  }
 
   return (
-    <Link href="/">
+    <Link href='/'>
       <motion.span
-        initial="hidden"
-        animate="visible"
+        initial='hidden'
+        animate='visible'
         variants={logoVariants}
-        className="text-sm font-semibold cursor-pointer "
+        className='text-sm font-semibold cursor-pointer '
       >
-        Philip Jon Echavez Lewis
+        Philip Lewis
       </motion.span>
     </Link>
-  );
-};
+  )
+}
 
-export default Logo;
+export default Logo
